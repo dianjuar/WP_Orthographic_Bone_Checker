a625b05b12d8e4d5390f1af6f80a11271cfbcb36
dianjuar <dianjuar@gmail.com>
2017-02-25 21:11:28 -0400

    Improving custom enchant filters

--- python/spellchecker/custom_enchant_filters.py
+++ python/spellchecker/custom_enchant_filters.py
@@ -4,14 +4,30 @@ import re
 
 from enchant.tokenize import Filter
 
+
 class HtmlEntitiesFilter(Filter):
-    """Filter skipping some HTML entities.
+    """Filter ignore HTML entities.
         Apply:
-            &SOMETHING;
+            WhatEver&SomeThing;WhatEver
     This filter skips any words matching the following regular expression:
-        &[\w]*;
+        [\w]*&[\w]*;[\w]*
     """
-    _pattern = re.compile(r"&[\w]*")
+    _pattern = re.compile(r"[\w]*&[\w]*[\w]*")
+
+    def _skip(self, word):
+        if self._pattern.match(word):
+            return True
+        return False
+
+
+class sprintfParametersFilter(Filter):
+    """Filter ignore php sprintf params.
+        Apply:
+            %NUMBER$s
+    This filter skips any words matching the following regular expression:
+        %[\0-9]+\$s
+    """
+    _pattern = re.compile(r"%[\0-9]+\$s")
 
     def _skip(self, word):
         if self._pattern.match(word):
