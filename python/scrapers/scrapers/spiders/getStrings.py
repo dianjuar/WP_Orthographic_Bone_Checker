7828475368a3cfc5afe37b91af5d5bf83b379e73
dianjuar <dianjuar@gmail.com>
2017-02-25 21:04:53 -0400

    Getting better the url of the spelling word.
    I'm using VIM, and it delete all unnecessary spaces.
    A lot of pep8 code standards

--- python/scrapers/scrapers/spiders/getStrings.py
+++ python/scrapers/scrapers/spiders/getStrings.py
@@ -2,9 +2,9 @@
 import scrapy
 import pdb
 
-#-items
+# -items
 # Import the item right. For test purposes this spider will call in several ways
-# Using this try I catch all possibilities   
+# Using this try I catch all possibilities
 # String to analize
 try:
     from scrapers.scrapers.items import item_stringToAnalize
@@ -15,7 +15,7 @@ try:
     from scrapers.scrapers.items import item_numberOfPages
 except ImportError:
     from scrapers.items import item_numberOfPages
-#-items
+# -items
 
 
 class GetstringsSpider(scrapy.Spider):
@@ -24,16 +24,16 @@ class GetstringsSpider(scrapy.Spider):
     start_urls = []
 
     '''
-    Activate several events if is set True for debugging or test proposes 
+    Activate several events if is set True for debugging or test proposes
     '''
     debug = False
     # debug = True
 
-    def __init__(self, WPTpage=None,*args, **kwargs):
+    def __init__(self, WPTpage=None, *args, **kwargs):
         '''
         @brief the constructor of the spider
 
-        @param  WPTpage 
+        @param  WPTpage
                 the page stract the strings
         @param args
         @param kwargs
@@ -43,8 +43,9 @@ class GetstringsSpider(scrapy.Spider):
         # Constructor
         super(GetstringsSpider, self).__init__(*args, **kwargs)
 
-        # load the start_urls received via parameter, if not passed, the url of the WP development project in spanish is set
-        if ( WPTpage == None ):
+        # Load the start_urls received via parameter, if not passed, the url of
+        # the WP development project in spanish is set
+        if (WPTpage is None):
             self.start_urls = ['https://translate.wordpress.org/projects/wp/dev/es/default']
         else:
             self.start_urls = [WPTpage]
@@ -53,11 +54,11 @@ class GetstringsSpider(scrapy.Spider):
         @brief N pages to scrap
         '''
         self.nPages = -1
-    
+
     def parse(self, response):
 
         # ---------------- Get the number of pages to scrap ON
-        hxs = scrapy.Selector( response )
+        hxs = scrapy.Selector(response)
         nPagesToScrap = hxs.xpath('''//div[@class="gp-content"]//
                                         div[@class="paging"]/
                                             *[@class="next"]/
@@ -65,7 +66,7 @@ class GetstringsSpider(scrapy.Spider):
                                                 text()''')
 
         # If debug is set, nPages = 10
-        self.nPages = int( nPagesToScrap.extract_first() ) if self.debug is False else 5
+        self.nPages = int(nPagesToScrap.extract_first()) if self.debug is False else 5
 
         item_npages = item_numberOfPages()
         item_npages['numberOfPages'] = self.nPages
@@ -77,13 +78,16 @@ class GetstringsSpider(scrapy.Spider):
         # Create all the url to scrap
         for npage in range(2, self.nPages):
             # 'filters[status]=current&' search by status current only
-            stringsUrl.append( self.start_urls[0]+'?filters[status]=current&page='+str( npage) )
-        
+            stringsUrl.append(self.start_urls[0] +
+                              '?filters[status]=current' +
+                              '&sort[by]=priority&sort[how]=desc&sorts=Sort' +
+                              '&page='+str(npage))
+
         # ---------------- Construct all the urls OFF
 
         # Visit all the urls
         for url in stringsUrl:
-            yield scrapy.Request(url=url, 
+            yield scrapy.Request(url=url,
                                  callback=self.getStrings)
 
         pass
@@ -92,22 +96,25 @@ class GetstringsSpider(scrapy.Spider):
         '''
         Get all the strings needed to analyse, if they have an error.
         '''
-        hxs = scrapy.Selector( response )
+        hxs = scrapy.Selector(response)
 
         # With this we get all the table's rows in the page visited
-        translatedRows = hxs.xpath('''//table[@id="translations"]/
-                                        tr[ contains(@class, "status-current") ]/
-                                            td[ contains(@class, "foreign-text")]''')
+        translatedRows = hxs.xpath('''
+                                   //table[@id="translations"]/
+                                        tr[contains(@class, "status-current")]/
+                                            td[contains(@class, "foreign-text")]
+                                   ''')
         # Iterate each row
         for rows in translatedRows:
             # Get the string
             stringToAnalize = rows.xpath('./text()').extract_first()
 
             # Scrapy Item
-            string_to_analize_item = item_stringToAnalize() 
+            string_to_analize_item = item_stringToAnalize()
             # Set the item values
-            string_to_analize_item['string'] = stringToAnalize.strip() # Strip is used to delete all white spaces, tabs and break lines
-            string_to_analize_item['url']    = response.url
+            # Strip is used to delete all white spaces, tabs and break lines
+            string_to_analize_item['string'] = stringToAnalize.strip()
+            string_to_analize_item['url'] = response.url
 
             # release the string scraped
             yield string_to_analize_item
