b20b18abd9506fb5a3671805f13a318d2040409b
dianjuar <dianjuar@gmail.com>
2017-02-25 21:07:06 -0400

    Bugfixing.
    Fixing the relative path, used when the scraper is called using the east way spider executor `scrapy crawl spider`
    
    Pep8 standars

--- python/scrapers/scrapers/__init__.py
+++ python/scrapers/scrapers/__init__.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 
-# Import the main folder of the project to acces spellchecker module
-sys.path.append( os.path.join( os.getcwd(), '..', '..')  )
+# Import the main folder of the project to access spellchecker module
+sys.path.append(os.path.join(os.getcwd(), '..'))
